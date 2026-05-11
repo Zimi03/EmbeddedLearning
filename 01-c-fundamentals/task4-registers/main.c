4ac6393e74e9911a87656c755c48390f4e99602f
Zimi03 <sz.senderowski@gmail.com>
2026-05-11 23:37:25 +0200

    added: task4 - register simulations on STM32 turning on clock on port C and configuring PC13 as output

---
+++ 01-c-fundamentals/task4-registers/main.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdint.h>
+
+#define BIT4 (1UL << 4)
+#define BIT20 (1UL << 20)
+#define BIT21 (1UL << 21)
+#define BIT22 (1UL << 22)
+#define BIT23 (1UL << 23)
+
+int main(){
+
+    uint32_t RCC_APB2ENR = 0;
+    uint32_t GPIO_CRH = 0;
+
+    RCC_APB2ENR |= BIT4;
+
+    GPIO_CRH &= ~BIT20;
+    GPIO_CRH &= ~BIT21;
+    GPIO_CRH &= ~BIT22;
+    GPIO_CRH &= ~BIT23;
+    GPIO_CRH |= BIT20;
+    
+
+    printf("0x%08X\n", RCC_APB2ENR);
+    printf("0x%08X\n", GPIO_CRH);
+
+    return 0;
+}
